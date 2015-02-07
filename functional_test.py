4bcda34952747505dc3e6685d7cd7343f48af0c4
filipedc82 <fildc@web.de>
2015-02-08 00:28:17 +0100

    Extended functional test

--- functional_test.py
+++ functional_test.py
@@ -20,12 +20,14 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('Orders', header_text)
 
         # On the page there is a button for creating a new order
-        self.
-
-
+        create_button = self.browser.find_element_by_id('create_order_button')
+        self.assertEqual(create_button.get_attribute('text'),'Create new order')
         # He hits the button to create a new order
+        create_button.click()
 
         # A new page opens with the title mentioning "Add new order"
+        url = self.browser.current_url
+        self.assertEqual(url,'\order\add' )
 
         # A text field is showing prompting to enter the customer name
 
