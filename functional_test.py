38a8daa1a06281b9633bbdfe9a1e139e6cb0a7e5
filipedc82 <fildc@web.de>
2015-02-08 12:05:23 +0100

    changed button to link

--- functional_test.py
+++ functional_test.py
@@ -19,16 +19,15 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Orders', header_text)
 
-        # On the page there is a button for creating a new order
-        create_button = self.browser.find_element_by_id('create_order_button')
-        self.assertEqual(create_button.get_attribute('type'),'button')
-        self.assertEqual(create_button.text,'Create new order')
+        # On the page there is a link for creating a new order
+        create_link = self.browser.find_element_by_id('create_order_link')
+        self.assertEqual(create_link.text,'Create new order')
         # He hits the button to create a new order
-        create_button.click()
+        self.browser.get(create_link.get_attribute('href'))
 
         # A new page opens with the title mentioning "Add new order"
         url = self.browser.current_url
-        self.assertEqual(url,'\order\add' )
+        self.assertRegex(url,r'^/order/add/')
 
         # A text field is showing prompting to enter the customer name
 
